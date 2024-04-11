1ab07d6ae5a817e95db52dc7d941f55409b5284f
muxbe <159569051+muxbe@users.noreply.github.com>
2024-04-11 22:29:51 +0200

    plus?

--- my.js
+++ my.js
@@ -19,7 +19,7 @@ function kvadrati(c) {
 kvadrati(3);
 let rendom = Math.floor(Math.random() * 100 + 0);
 console.log(rendom);
-
+//with "for"
 //let onenikname = [];
 //for (let i = 0; i < characters.length; i++) {
 //if (characters[i].alternate_names.length === 0) {
@@ -27,7 +27,20 @@ console.log(rendom);
 //}
 //}
 //console.log(onenikname);
+
 let niknamesfree = characters.filter(function (e) {
   return e.alternate_names.length === 0;
 });
 console.log(niknamesfree);
+//function2???
+//let niknamesFree = characters.filter(
+// (character) => characters.alternate_names.lenght === 0
+//);
+
+//console.log(niknamesFree);
+let date = new Date();
+let year = date.getFullYear();
+let age = characters.filter(function (e) {
+  return year - e.dateOfBirth >= 18;
+});
+console.log(age);
